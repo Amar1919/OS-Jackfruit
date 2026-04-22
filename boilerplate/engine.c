82ac63c8f0d4d023efab95b719248f569236e598
Amar <nawadagiamar@gmail.com>
2026-04-21 22:05:56 -0400

    Increase MAX containers and improve logging
    Updated the maximum number of containers from 20 to 50. Modified container logging and management functions for better performance and clarity.

--- boilerplate/engine.c
+++ boilerplate/engine.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <signal.h>
 
-#define MAX 20
+#define MAX 50
 
 typedef struct {
     char id[50];
@@ -15,34 +15,29 @@ typedef struct {
 Container c[MAX];
 int count = 0;
 
-// find container
-int find(char *id) {
-    for(int i = 0; i < count; i++) {
-        if(strcmp(c[i].id, id) == 0)
-            return i;
-    }
-    return -1;
-}
-
-// supervisor
-void run_supervisor() {
-    printf("Supervisor started...\n");
-    while(1) {
-        sleep(5);
-    }
-}
-
 // start container
 void start_container(char *id, char *rootfs, char *cmd) {
     pid_t pid = fork();
 
-    if(pid == 0) {
+    if (pid == 0) {
+        // child process (container)
         chroot(rootfs);
         chdir("/");
-        execl(cmd, cmd, "-c", "while true; do sleep 5; done", NULL);
+
+        // logging inside container
+        FILE *f = fopen("/log.txt", "w");
+        if (f) {
+            dup2(fileno(f), STDOUT_FILENO);
+            dup2(fileno(f), STDERR_FILENO);
+        }
+
+        // infinite loop to keep container alive
+        execl(cmd, cmd, "-c", "while true; do echo running; sleep 2; done", NULL);
+
         perror("exec failed");
         exit(1);
     } else {
+        // parent process
         strcpy(c[count].id, id);
         c[count].pid = pid;
         c[count].running = 1;
@@ -52,56 +47,49 @@ void start_container(char *id, char *rootfs, char *cmd) {
     }
 }
 
-// list containers
+// list containers (using system process table)
 void list_containers() {
-    printf("Running container processes:\n");
-    system("ps -ef | grep sh | grep -v grep");
+    printf("Container Processes:\n");
+    system("ps -ef | grep 'while true' | grep -v grep");
 }
 
 // stop container
 void stop_container(char *id) {
-    int idx = find(id);
-
-    if(idx == -1) {
-        printf("Container not found\n");
-        return;
+    for (int i = 0; i < count; i++) {
+        if (strcmp(c[i].id, id) == 0 && c[i].running) {
+            kill(c[i].pid, SIGTERM);
+            c[i].running = 0;
+            printf("Container %s stopped\n", id);
+            return;
+        }
     }
-
-    kill(c[idx].pid, SIGTERM);
-    c[idx].running = 0;
-
-    printf("Container %s stopped\n", id);
+    printf("Container not found\n");
 }
 
 int main(int argc, char *argv[]) {
 
-    if(argc < 2) {
+    if (argc < 2) {
         printf("Usage:\n");
-        printf("./engine supervisor\n");
         printf("./engine start <id> <rootfs> <cmd>\n");
         printf("./engine ps\n");
         printf("./engine stop <id>\n");
         return 1;
     }
 
-    if(strcmp(argv[1], "supervisor") == 0) {
-        run_supervisor();
-    }
-
-    else if(strcmp(argv[1], "start") == 0) {
-        if(argc < 5) {
+    if (strcmp(argv[1], "start") == 0) {
+        if (argc < 5) {
             printf("Usage: start <id> <rootfs> <cmd>\n");
             return 1;
         }
         start_container(argv[2], argv[3], argv[4]);
     }
 
-    else if(strcmp(argv[1], "ps") == 0) {
+    else if (strcmp(argv[1], "ps") == 0) {
         list_containers();
     }
 
-    else if(strcmp(argv[1], "stop") == 0) {
-        if(argc < 3) {
+    else if (strcmp(argv[1], "stop") == 0) {
+        if (argc < 3) {
             printf("Usage: stop <id>\n");
             return 1;
         }

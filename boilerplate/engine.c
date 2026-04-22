a1137a03979c571c2b0b5ad952d7af10306cb00a
Amar <nawadagiamar@gmail.com>
2026-04-21 22:53:01 -0400

    Add supervisor mode and improve container management

--- boilerplate/engine.c
+++ boilerplate/engine.c
@@ -15,29 +15,44 @@ typedef struct {
 Container c[MAX];
 int count = 0;
 
-// start container
+// -------------------- SUPERVISOR --------------------
+void run_supervisor() {
+    printf("Supervisor started...\n");
+
+    while (1) {
+        sleep(5);
+        printf("Supervisor is monitoring containers...\n");
+    }
+}
+
+// -------------------- START CONTAINER --------------------
 void start_container(char *id, char *rootfs, char *cmd) {
     pid_t pid = fork();
 
     if (pid == 0) {
-        // child process (container)
-        chroot(rootfs);
-        chdir("/");
+        // child process
+        if (chroot(rootfs) != 0) {
+            perror("chroot failed");
+            exit(1);
+        }
 
-        // logging inside container
+        if (chdir("/") != 0) {
+            perror("chdir failed");
+            exit(1);
+        }
+
+        // logging
         FILE *f = fopen("/log.txt", "w");
         if (f) {
             dup2(fileno(f), STDOUT_FILENO);
             dup2(fileno(f), STDERR_FILENO);
         }
 
-        // infinite loop to keep container alive
         execl(cmd, cmd, "-c", "while true; do echo running; sleep 2; done", NULL);
 
         perror("exec failed");
         exit(1);
     } else {
-        // parent process
         strcpy(c[count].id, id);
         c[count].pid = pid;
         c[count].running = 1;
@@ -47,13 +62,13 @@ void start_container(char *id, char *rootfs, char *cmd) {
     }
 }
 
-// list containers (using system process table)
+// -------------------- LIST CONTAINERS --------------------
 void list_containers() {
-    printf("Container Processes:\n");
-    system("ps -ef | grep 'while true' | grep -v grep");
+    printf("Running Container Processes:\n");
+    system("ps -ef | grep '/bin/sh -c while true' | grep -v grep");
 }
 
-// stop container
+// -------------------- STOP CONTAINER --------------------
 void stop_container(char *id) {
     for (int i = 0; i < count; i++) {
         if (strcmp(c[i].id, id) == 0 && c[i].running) {
@@ -66,17 +81,26 @@ void stop_container(char *id) {
     printf("Container not found\n");
 }
 
+// -------------------- MAIN --------------------
 int main(int argc, char *argv[]) {
 
     if (argc < 2) {
         printf("Usage:\n");
+        printf("./engine supervisor\n");
         printf("./engine start <id> <rootfs> <cmd>\n");
         printf("./engine ps\n");
         printf("./engine stop <id>\n");
         return 1;
     }
 
-    if (strcmp(argv[1], "start") == 0) {
+    // supervisor mode
+    if (strcmp(argv[1], "supervisor") == 0) {
+        run_supervisor();
+        return 0;
+    }
+
+    // start container
+    else if (strcmp(argv[1], "start") == 0) {
         if (argc < 5) {
             printf("Usage: start <id> <rootfs> <cmd>\n");
             return 1;
@@ -84,10 +108,12 @@ int main(int argc, char *argv[]) {
         start_container(argv[2], argv[3], argv[4]);
     }
 
+    // list containers
     else if (strcmp(argv[1], "ps") == 0) {
         list_containers();
     }
 
+    // stop container
     else if (strcmp(argv[1], "stop") == 0) {
         if (argc < 3) {
             printf("Usage: stop <id>\n");
